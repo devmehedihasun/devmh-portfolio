2ebbe4ef5c80898de35e172f0bdcbbebe06057bc
Mehedi Hasan <117835267+devmehedihasun@users.noreply.github.com>
2025-11-02 20:21:19 +0600

    update content

--- components/sections/Experience2.tsx
+++ components/sections/Experience2.tsx
@@ -17,12 +17,12 @@ export default function Experience2() {
 									<span className="text-linear-4 d-flex align-items-center"> Expericence </span>
 								</div>
 								<h3>
-									+12
+									+3
 									<span className="text-300">years of </span>
 									passion
 									<span className="text-300">
 										for <br />
-										programming techniques
+										WordPress & Web Development
 									</span>
 								</h3>
 								<div className="row mt-5">
@@ -32,8 +32,8 @@ export default function Experience2() {
 												<div className="d-flex align-items-center gap-2">
 													<img src="assets/imgs/home-page-2/experience/google.svg" alt="zelio" />
 													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Google</h5>
-														<span className="text-300">2018 - Present</span>
+														<h5 className="mb-1">Backbencher Studio</h5>
+														<span className="text-300">2024 - Present</span>
 													</div>
 												</div>
 											</Link>
@@ -41,8 +41,8 @@ export default function Experience2() {
 												<div className="d-flex align-items-center gap-2">
 													<img src="assets/imgs/home-page-2/experience/twitter.svg" alt="zelio" />
 													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Twitter (X)</h5>
-														<span className="text-300">2012 - 2015</span>
+														<h5 className="mb-1">2024</h5>
+														<span className="text-300"></span>
 													</div>
 												</div>
 											</Link>
@@ -50,8 +50,8 @@ export default function Experience2() {
 												<div className="d-flex align-items-center gap-2">
 													<img src="assets/imgs/home-page-2/experience/amazon.svg" alt="zelio" />
 													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">Amazon</h5>
-														<span className="text-300">2018 - Present</span>
+														<h5 className="mb-1">Remot</h5>
+														<span className="text-300">2023-2024</span>
 													</div>
 												</div>
 											</Link>
@@ -59,26 +59,26 @@ export default function Experience2() {
 												<div className="d-flex align-items-center gap-2">
 													<img src="assets/imgs/home-page-2/experience/payPal.svg" alt="zelio" />
 													<div className="d-flex flex-column ms-2">
-														<h5 className="mb-1">PayPal</h5>
-														<span className="text-300">2010 - 2012</span>
+														<h5 className="mb-1">Softvence</h5>
+														<span className="text-300">2022 - 2023</span>
 													</div>
 												</div>
 											</Link>
 										</div>
 									</div>
 									<div className="col-lg-8 ps-lg-5 mt-5 mt-lg-0">
-										<h6 className="text-linear-4">Senior Software Engineer</h6>
+										<h6 className="text-linear-4">WordPress Developer</h6>
 										<ul className="mt-4">
-											<li className="text-dark mb-3">Led development of scalable web applications, <span className="text-secondary-2">improving performance</span> and user experience for millions of users.</li>
-											<li className="text-dark mb-3">Implemented machine learning algorithms to enhance search functionality.</li>
-											<li className="text-dark mb-3">Collaborated with cross-functional teams to integrate new features seamlessly.</li>
+											<li className="text-dark mb-3">Built and customized dynamic, high-performing WordPress websites for global clients, <span className="text-secondary-2">ensuring fast performance</span> and seamless user experience.</li>
+											<li className="text-dark mb-3">Developed custom themes and plugins to extend site functionality and deliver tailored solutions.</li>
+											<li className="text-dark mb-3">Collaborated with design and content teams to convert Figma layouts into fully responsive WordPress sites.</li>
 										</ul>
 										<div className="d-flex flex-wrap align-items-center gap-3 mt-7">
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Python</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">TensorFlow</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Angular</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">Kubernetes</Link>
-											<Link href="#" className="text-300 border border-1 px-3 py-1">GCP</Link>
+											<Link href="#" className="text-300 border border-1 px-3 py-1">Elementor</Link>
+											<Link href="#" className="text-300 border border-1 px-3 py-1">ACF</Link>
+											<Link href="#" className="text-300 border border-1 px-3 py-1">Crocoblock</Link>
+											<Link href="#" className="text-300 border border-1 px-3 py-1">JavaScript</Link>
+											<Link href="#" className="text-300 border border-1 px-3 py-1">PHP</Link>
 										</div>
 									</div>
 								</div>
