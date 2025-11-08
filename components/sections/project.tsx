7f0e69ed029fa17093aee429fb9ff4f69fcbb1cc
Mehedi Hasan <117835267+devmehedihasun@users.noreply.github.com>
2025-11-08 23:01:26 +0600

    added some project

--- components/sections/project.tsx
+++ components/sections/project.tsx
@@ -33,5 +33,38 @@ export const projects: ProjectType[] = [
     websitelink: "https://brightpathaisolutions.com/",
     githubLink: "#"
   },
+    {
+    id: 3,
+    image: "assets/imgs/home-page-2/projects/real.png",
+    title: "real estate website",
+    description: "Real estate website for a local agency for marketing.",
+    client: "Robert",
+    completionTime: "5 days",
+    technologies: ["WordPress", "Elementor", "Javascript", "PHP"],
+    websitelink: "https://clawsonlegacygroup.com/",
+    githubLink: "#"
+  },
+   {
+    id: 4,
+    image: "assets/imgs/home-page-2/projects/evidenceandbeyond.png",
+    title: "Oragnization website",
+    description: "globally engaged consultancy advancing evidence-informed solutions.",
+    client: "Lina Digolo",
+    completionTime: "15 days",
+    technologies: ["WordPress", "Elementor", "Javascript", "ACF","PHP"],
+    websitelink: "https://evidenceandbeyond.org/",
+    githubLink: "#"
+  },
+   {
+    id: 5,
+    image: "assets/imgs/home-page-2/projects/evidenceandbeyond.png",
+    title: "Non-profit website",
+    description: "non-pofit safe animals organization website.",
+    client: "clinta ",
+    completionTime: "3 days emegency project",
+    technologies: ["WordPress", "Elementor", "Javascript","PHP"],
+    websitelink: "https://scienceforcowildlife.org/",
+    githubLink: "#"
+  },
   // Add more projects as needed
 ];
